b2d309e2875b10f6bd32fe95f8c28c3309f843c2
0xf3cd <nq.maigre@gmail.com>
2024-05-28 22:09:11 -0700

    Refactored a bit further

--- src/Analyzer/Relationship.py
+++ src/Analyzer/Relationship.py
@@ -5,13 +5,29 @@ import copy
 from itertools import starmap, product, compress, chain
 from typing import Final, TypedDict, Callable, Union, Iterable
 
+from ..Common import PillarData
 from ..Defines import Tiangan, Dizhi
 from ..BaziChart import BaziChart
 from ..Utils import ShenshaUtils, TianganUtils, DizhiUtils
 from ..Discoverer.GanzhiDiscoverer import GanzhiDiscoverer
 
 
-class RelationshipAnalysis(TypedDict):
+_FirstArgType = Iterable[Union[Tiangan, Dizhi]]
+_SecondArgType = Iterable[Dizhi]
+_ArgsType = tuple[_FirstArgType, _SecondArgType]
+
+def find_shensha(
+  f: Callable[..., bool],
+  *args: _ArgsType,
+) -> Iterable[Dizhi]:
+  '''An fp-styled helper private/internal function for finding Shensha (神煞).'''
+  producted_args = list(chain(*map(lambda a : product(*a), args)))
+  results = starmap(f, producted_args)
+  return map(lambda x : x[1], compress(producted_args, results))
+
+
+
+class ShenshaAnalysis(TypedDict):
   # The Taohua Dizhis   (桃花星所在地支)
   taohua:   frozenset[Dizhi]
   # The Hongyan Dizhis  (红艳星所在地支)
@@ -21,59 +37,53 @@ class RelationshipAnalysis(TypedDict):
   # The Tianxi Dizhis   (天喜星所在地支)
   tianxi:   frozenset[Dizhi]
 
-  # The relations that the day master and other Tiangans form.
-  # 日主与其他天干形成的关系（合、冲等）
-  day_master_relations: TianganUtils.TianganRelationDiscovery
 
-  # The relations that the house of relationship and other Dizhis form.
-  # 夫妻宫与其他地支形成的关系（合、冲等）
-  house_relations: DizhiUtils.DizhiRelationDiscovery
+class AtBirthAnalysis:
+  '''Analysis of Relationship at Birth (出生时的亲密关系分析)'''
+  def __init__(self, chart: BaziChart) -> None:
+    self._chart: Final[BaziChart] = copy.deepcopy(chart)
+    self._ganzhi_discoverer: Final[GanzhiDiscoverer] = GanzhiDiscoverer(self._chart)
 
-  # The relations that relationship stars and other Tiangans form.
-  # 夫妻星与其他天干形成的关系（合、冲等）
-  tg_star_relations: TianganUtils.TianganRelationDiscovery
+  @property
+  def shensha(self) -> ShenshaAnalysis:
+    dm = self._chart.bazi.day_master
+    y_dz, m_dz, d_dz, h_dz = self._chart.bazi.four_dizhis
+    return {
+      'taohua' :  frozenset(find_shensha(ShenshaUtils.taohua,   ([y_dz],  [m_dz, d_dz, h_dz]), 
+                                                                ([d_dz],  [y_dz, m_dz, h_dz]))),
+      'hongyan':  frozenset(find_shensha(ShenshaUtils.hongyan,  ([dm],    [y_dz, m_dz, d_dz, h_dz]))),
+      'hongluan': frozenset(find_shensha(ShenshaUtils.hongluan, ([y_dz],  [m_dz, d_dz, h_dz]))),
+      'tianxi':   frozenset(find_shensha(ShenshaUtils.tianxi,   ([y_dz],  [m_dz, d_dz, h_dz]))),
+    }
 
-  # The relations that relationship stars and other Dizhis form.
-  # 夫妻星与其他地支形成的关系（合、冲等）
-  dz_star_relations: DizhiUtils.DizhiRelationDiscovery
+  @property
+  def day_master_relations(self) -> TianganUtils.TianganRelationDiscovery:
+    y_tg, m_tg, d_tg, h_tg = self._chart.bazi.four_tiangans
+    return TianganUtils.discover_mutual([d_tg], [y_tg, m_tg, h_tg])
+  
+  @property
+  def house_relations(self) -> DizhiUtils.DizhiRelationDiscovery:
+    '''Relations that the House of Relationship / 婚姻宫 has.'''
+    y_dz, m_dz, d_dz, h_dz = self._chart.bazi.four_dizhis
+    return DizhiUtils.discover_mutual([d_dz], [y_dz, m_dz, h_dz])
+  
+  @property
+  def star_relations(self) -> PillarData[TianganUtils.TianganRelationDiscovery, DizhiUtils.DizhiRelationDiscovery]:
+    '''Relations that the Star of Relationship / 配偶星 / 婚姻星 has.'''
+    stars = self._chart.relationship_stars
+    at_birth_discovery = self._ganzhi_discoverer.at_birth
+
+    tg = at_birth_discovery.tiangan.filter(lambda _, combo : stars.tiangan in combo)
+    dz = at_birth_discovery.dizhi.filter(lambda _, combo : any(dz in combo for dz in stars.dizhi))
+    return PillarData(tg, dz)
+
 
 
 class RelationshipAnalyzer:
-  _FirstArgType = Iterable[Union[Tiangan, Dizhi]]
-  _SecondArgType = Iterable[Dizhi]
-  _ArgsType = tuple[_FirstArgType, _SecondArgType]
-  @staticmethod
-  def __find_shensha(
-    f: Callable[..., bool],
-    *args: _ArgsType,
-  ) -> Iterable[Dizhi]:
-    '''An fp-styled helper private method for finding Shensha (神煞).'''
-    producted_args = list(chain(*map(lambda a : product(*a), args)))
-    results = starmap(f, producted_args)
-    return map(lambda x : x[1], compress(producted_args, results))
-  
+  '''A thin wrapper of `AtBirthAnalysis` and `TransitAnalysis`.'''
   def __init__(self, bazi_chart: BaziChart) -> None:
-    self._bazi_chart: Final[BaziChart] = copy.deepcopy(bazi_chart)
-    self._ganzhi_discoverer: Final[GanzhiDiscoverer] = GanzhiDiscoverer(self._bazi_chart)
+    self._chart: Final[BaziChart] = copy.deepcopy(bazi_chart)
 
   @property
-  def at_birth(self) -> RelationshipAnalysis:
-    y_tg, m_tg, d_tg, h_tg = self._bazi_chart.bazi.four_tiangans
-    y_dz, m_dz, d_dz, h_dz = self._bazi_chart.bazi.four_dizhis
-    
-    stars = self._bazi_chart.relationship_stars
-    at_birth_discovery = self._ganzhi_discoverer.at_birth
-
-    return {
-      'taohua' :  frozenset(self.__find_shensha(ShenshaUtils.taohua,   ([y_dz],  [m_dz, d_dz, h_dz]), 
-                                                                       ([d_dz],  [y_dz, m_dz, h_dz]))),
-      'hongyan':  frozenset(self.__find_shensha(ShenshaUtils.hongyan,  ([d_tg],  [y_dz, m_dz, d_dz, h_dz]))),
-      'hongluan': frozenset(self.__find_shensha(ShenshaUtils.hongluan, ([y_dz],  [m_dz, d_dz, h_dz]))),
-      'tianxi':   frozenset(self.__find_shensha(ShenshaUtils.tianxi,   ([y_dz],  [m_dz, d_dz, h_dz]))),
-
-      'day_master_relations': TianganUtils.discover_mutual([d_tg], [y_tg, m_tg, h_tg]),
-      'house_relations': DizhiUtils.discover_mutual([d_dz], [y_dz, m_dz, h_dz]),
-
-      'tg_star_relations': at_birth_discovery.tiangan.filter(lambda _, combo : stars.tiangan in combo),
-      'dz_star_relations': at_birth_discovery.dizhi.filter(lambda _, combo : any(dz in combo for dz in stars.dizhi)),
-    }
+  def at_birth(self) -> AtBirthAnalysis:
+    return AtBirthAnalysis(self._chart)
--- tests/analyzer/test_relationship_analyzer.py
+++ tests/analyzer/test_relationship_analyzer.py
@@ -12,22 +12,17 @@ from src.BaziChart import BaziChart
 from src.Analyzer.Relationship import RelationshipAnalyzer
 
 
-class TestRelationshipAnalyzer(unittest.TestCase):
-  def test_at_birth_misc(self) -> None:
-    chart = BaziChart.random()
-    analyzer = RelationshipAnalyzer(chart)
-
-    self.assertEqual(analyzer.at_birth, analyzer.at_birth, 'Equality')
-    self.assertEqual(analyzer.at_birth, analyzer.at_birth, 'Constancy')
-
+class TestAtBirthAnalysis(unittest.TestCase):
   @pytest.mark.slow
-  def test_at_birth(self) -> None:
+  def test_shensha(self) -> None:
     for _ in range(100):
       chart = BaziChart.random()
       analyzer = RelationshipAnalyzer(chart)
 
       dm: Tiangan = chart.bazi.day_master
       y, m, d, h = chart.bazi.four_dizhis
+
+      at_birth = analyzer.at_birth
 
       with self.subTest('Taohua / 桃花'):
         expected_taohua: list[Dizhi] = []
@@ -37,81 +32,105 @@ class TestRelationshipAnalyzer(unittest.TestCase):
         for dz1, dz2 in itertools.product([d], [y, m, h]):
           if ShenshaUtils.taohua(dz1, dz2):
             expected_taohua.append(dz2)
-        self.assertSetEqual(analyzer.at_birth['taohua'], set(expected_taohua))
-        self.assertSetEqual(analyzer.at_birth['taohua'], analyzer.at_birth['taohua'], 'Constancy')
+        self.assertSetEqual(at_birth.shensha['taohua'], set(expected_taohua))
+        self.assertSetEqual(at_birth.shensha['taohua'], at_birth.shensha['taohua'], 'Constancy')
 
       with self.subTest('Hongyan / 红艳'):
         expected_hongyan: list[Dizhi] = []
         for tg, dz in itertools.product([dm], [y, m, d, h]):
           if ShenshaUtils.hongyan(tg, dz):
             expected_hongyan.append(dz)
-        self.assertSetEqual(analyzer.at_birth['hongyan'], set(expected_hongyan))
-        self.assertSetEqual(analyzer.at_birth['hongyan'], analyzer.at_birth['hongyan'], 'Constancy')
+        self.assertSetEqual(at_birth.shensha['hongyan'], set(expected_hongyan))
+        self.assertSetEqual(at_birth.shensha['hongyan'], at_birth.shensha['hongyan'], 'Constancy')
 
       with self.subTest('Hongluan / 红鸾'):
         expected_hongluan: list[Dizhi] = []
         for dz1, dz2 in itertools.product([y], [m, d, h]):
           if ShenshaUtils.hongluan(dz1, dz2):
             expected_hongluan.append(dz2)
-        self.assertSetEqual(analyzer.at_birth['hongluan'], set(expected_hongluan))
-        self.assertSetEqual(analyzer.at_birth['hongluan'], analyzer.at_birth['hongluan'], 'Constancy')
+        self.assertSetEqual(at_birth.shensha['hongluan'], set(expected_hongluan))
+        self.assertSetEqual(at_birth.shensha['hongluan'], at_birth.shensha['hongluan'], 'Constancy')
 
       with self.subTest('Tianxi / 天喜'):
         expected_tianxi: list[Dizhi] = []
         for dz1, dz2 in itertools.product([y], [m, d, h]):
           if ShenshaUtils.tianxi(dz1, dz2):
             expected_tianxi.append(dz2)
-        self.assertSetEqual(analyzer.at_birth['tianxi'], set(expected_tianxi))
-        self.assertSetEqual(analyzer.at_birth['tianxi'], analyzer.at_birth['tianxi'], 'Constancy')
+        self.assertSetEqual(at_birth.shensha['tianxi'], set(expected_tianxi))
+        self.assertSetEqual(at_birth.shensha['tianxi'], at_birth.shensha['tianxi'], 'Constancy')
 
-      with self.subTest('House of Relationship / 婚姻宫'):
-        self.assertEqual(analyzer.at_birth['house_relations'], DizhiUtils.discover_mutual([y, m, h], [d]))
-        self.assertEqual(analyzer.at_birth['house_relations'], analyzer.at_birth['house_relations'], 'Constancy')
+  @pytest.mark.slow
+  def test_day_master_relations(self) -> None:
+    for _ in range(100):
+      chart = BaziChart.random()
+      analyzer = RelationshipAnalyzer(chart)
 
-      with self.subTest('Day Master Relations / 日主关系'):
-        self.assertEqual(analyzer.at_birth['day_master_relations'], TianganUtils.discover_mutual([
-          chart.bazi.year_pillar.tiangan, 
-          chart.bazi.month_pillar.tiangan,
-          chart.bazi.hour_pillar.tiangan
-        ], [dm]))
-        self.assertEqual(analyzer.at_birth['day_master_relations'], analyzer.at_birth['day_master_relations'], 'Constancy')
+      dm = chart.bazi.day_master
+      at_birth = analyzer.at_birth
 
-      with self.subTest('Relationship Star Relations / 夫妻星关系'):
-        stars = chart.relationship_stars
-        for tg_combos in analyzer.at_birth['tg_star_relations'].values():
-          for tg_combo in tg_combos:
-            self.assertIn(stars.tiangan, tg_combo)
-        for dz_combos in analyzer.at_birth['dz_star_relations'].values():
-          for dz_combo in dz_combos:
-            self.assertTrue(any(dz in dz_combo for dz in stars.dizhi))
+      self.assertEqual(at_birth.day_master_relations, TianganUtils.discover_mutual([
+        chart.bazi.year_pillar.tiangan, 
+        chart.bazi.month_pillar.tiangan,
+        chart.bazi.hour_pillar.tiangan
+      ], [dm]))
+      self.assertEqual(at_birth.day_master_relations, at_birth.day_master_relations, 'Constancy')
 
-        self.assertEqual(analyzer.at_birth['tg_star_relations'], analyzer.at_birth['tg_star_relations'], 'Constancy')
-        self.assertEqual(analyzer.at_birth['dz_star_relations'], analyzer.at_birth['dz_star_relations'], 'Constancy')
+  @pytest.mark.slow
+  def test_house_relations(self) -> None:
+    for _ in range(100):
+      chart = BaziChart.random()
+      analyzer = RelationshipAnalyzer(chart)
 
+      y, m, d, h = chart.bazi.four_dizhis
+      at_birth = analyzer.at_birth
+
+      self.assertEqual(at_birth.house_relations, DizhiUtils.discover_mutual([y, m, h], [d]))
+      self.assertEqual(at_birth.house_relations, at_birth.house_relations, 'Constancy')
+
+  @pytest.mark.slow
+  def test_star_relations(self) -> None:
+    for _ in range(100):
+      chart = BaziChart.random()
+      analyzer = RelationshipAnalyzer(chart)
+
+      at_birth = analyzer.at_birth
+      stars = chart.relationship_stars
+      
+      for tg_combos in at_birth.star_relations.tiangan.values():
+        for tg_combo in tg_combos:
+          self.assertIn(stars.tiangan, tg_combo)
+      for dz_combos in at_birth.star_relations.dizhi.values():
+        for dz_combo in dz_combos:
+          self.assertTrue(any(dz in dz_combo for dz in stars.dizhi))
+
+      self.assertEqual(at_birth.star_relations.tiangan, at_birth.star_relations.tiangan, 'Constancy')
+      self.assertEqual(at_birth.star_relations.dizhi, at_birth.star_relations.dizhi, 'Constancy')
 
   @pytest.mark.slow
   def test_filtered(self) -> None:
-    '''Test "tg_star_relations" and "dz_star_relations" only contain filtered combos.'''
+    '''Test "star_relations" only contain filtered combos.'''
     for _ in range(16):
       chart: BaziChart = BaziChart.random()
       analyzer: RelationshipAnalyzer = RelationshipAnalyzer(chart)
-      stars = chart.relationship_stars
-      
-      with self.subTest('at_birth'):
-        at_birth_analysis = analyzer.at_birth
 
-        for tg_rel, tg_combos in TianganUtils.discover(chart.bazi.four_tiangans).items():
-          if tg_rel not in at_birth_analysis['tg_star_relations']:
-            self.assertTrue(all(stars.tiangan not in tg_combo for tg_combo in tg_combos))
-          else:
-            for tg_combo in tg_combos:
-              self.assertEqual(stars.tiangan in tg_combo,
-                               tg_combo in at_birth_analysis['tg_star_relations'][tg_rel])
-              
-        for dz_rel, dz_combos in DizhiUtils.discover(chart.bazi.four_dizhis).items():
-          if dz_rel not in at_birth_analysis['dz_star_relations']:
-            self.assertTrue(all(dz not in dz_combo for dz_combo in dz_combos for dz in stars.dizhi))
-          else:
-            for dz_combo in dz_combos:
-              self.assertEqual(any(dz in dz_combo for dz in stars.dizhi),
-                               dz_combo in at_birth_analysis['dz_star_relations'][dz_rel])
+      stars = chart.relationship_stars
+      at_birth = analyzer.at_birth
+
+      for tg_rel, tg_combos in TianganUtils.discover(chart.bazi.four_tiangans).items():
+        if tg_rel not in at_birth.star_relations.tiangan:
+          self.assertTrue(all(stars.tiangan not in tg_combo for tg_combo in tg_combos))
+        else:
+          for tg_combo in tg_combos:
+            self.assertEqual(stars.tiangan in tg_combo,
+                             tg_combo in at_birth.star_relations.tiangan[tg_rel])
+            
+      for dz_rel, dz_combos in DizhiUtils.discover(chart.bazi.four_dizhis).items():
+        if dz_rel not in at_birth.star_relations.dizhi:
+          self.assertTrue(all(dz not in dz_combo for dz_combo in dz_combos for dz in stars.dizhi))
+        else:
+          for dz_combo in dz_combos:
+            self.assertEqual(any(dz in dz_combo for dz in stars.dizhi),
+                             dz_combo in at_birth.star_relations.dizhi[dz_rel])
+
+
+# TODO: Integration tests on `RelationshipAnalyzer`.
